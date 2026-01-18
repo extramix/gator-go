e0c8c78a85179457ae33be65ea64c9416932f6ae
ミック(Sirapat) <sirapat.jr@gmail.com>
2026-01-18 16:26:16 +0900

    feat: add table

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+DROP TABLE users;
+
